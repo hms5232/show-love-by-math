8491d007944b2d9b9951977065c6ea0fef08a325
hms5232 <43672033+hms5232@users.noreply.github.com>
2022-07-07 12:42:10 +0800

    終於找到失蹤的下半身
    我居然忘記去平方要加正負號
    對不起數學老師

--- python/love.py
+++ python/love.py
@@ -12,5 +12,7 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(-1,1,100)
 y = ((1 - (x**2))**(1/2)) + (x**2)**(1/3)
+y2 = -((1 - (x**2))**(1/2)) + (x**2)**(1/3)  # 等式去平方開根號要加上正負號
 plt.plot(x,y)
+plt.plot(x,y2)
 plt.show()
